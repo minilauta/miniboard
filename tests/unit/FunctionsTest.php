df154cb80a32df143f101069432c5969b8c93a25
den4 <denamuumi@proton.me>
2022-09-25 11:56:04 +0300

    Fix according to PR comments

--- tests/unit/FunctionsTest.php
+++ tests/unit/FunctionsTest.php
@@ -173,7 +173,6 @@ final class FunctionsTest extends TestCase {
   public function test_truncate_message() {
     $message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     $truncated_message = truncate_message(message: $message, length: 10);
-    print_r($truncated_message);
     $this->assertEquals(13, strlen($truncated_message));
   }
 }
