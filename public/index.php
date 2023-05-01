e9dad82f938bcd4b698a4984a9456bd7bf704b30
anonyymi <anonyymigithub@gmail.com>
2023-05-01 22:47:08 +0300

    use ?mb_version=1234 query param to invalidate new build assets client-side cache

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/version.php';
 require_once __DIR__ . '/middleware.php';
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/config.php';
