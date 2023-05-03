7ce05906561ef592549b2ae1f64800fecb0faf77
anonyymi <anonyymigithub@gmail.com>
2023-05-03 13:56:42 +0300

    add element remove polyfill

--- public/js/polyfill.js
+++ public/js/polyfill.js
@@ -49,3 +49,20 @@ import 'whatwg-fetch';
     });
   });
 })([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+	arr.forEach(function (item) {
+		if (item.hasOwnProperty('remove')) {
+			return;
+		}
+		Object.defineProperty(item, 'remove', {
+			configurable: true,
+			enumerable: true,
+			writable: true,
+			value: function remove() {
+				this.parentNode.removeChild(this);
+			}
+		});
+	});
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
